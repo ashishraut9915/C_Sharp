4778d1f766f23ad9574d2263d67fb6aed85c306a
Ashish Raut <161839535+ashishraut9915@users.noreply.github.com>
2024-08-09 17:03:59 +0530

    Update Program.cs
    Done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace CSharp_Programming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Constants
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck= random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            { 
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
