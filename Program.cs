8e1acb837a6598d4320f03297e57fdb9d1b5663d
Ashish Raut <161839535+ashishraut9915@users.noreply.github.com>
2024-08-10 11:10:34 +0530

    Update Program.cs
    UC5 UPDATED

--- Program.cs
+++ Program.cs
@@ -4,9 +4,40 @@ namespace CSharp_Programming
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variables 
+            int emphrs = 0, empwage 0, total EmpWage-0;
+            //Computation 
+            for(int day 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random new Random(); 
+                int empCheck= random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS PART_TIME:
+                         empHrs = 4;
+                         break;
+
+                    case IS FULL_TIME:
+                         empHrs = 8;
+                         break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empwage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage:" + empilage);
+            }
+            Console.WriteLine("Total Emp Wage:" + totalEmpWage);
         }
     }
 }
