8344c6098028ca75ed519b87d3a90c0e27eefc21
Ashish Raut <161839535+ashishraut9915@users.noreply.github.com>
2024-08-10 10:48:37 +0530

    Update Program.cs
    UC3 Updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace CSharp_Programming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs 0;
+            int empwage 0;
+            Random random = new Random();
+            //Computation
+            int empCheck= random.Next(0, 3);
+            if (empCheck=- IS_PART_TIME)
+            { 
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            { 
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR; 
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
