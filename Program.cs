a6eaaf85c6206cda09b973bbda104b87ba563569
Ashish Raut <161839535+ashishraut9915@users.noreply.github.com>
2024-08-10 10:59:16 +0530

    Update Program.cs
    UC4 Updated

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@ namespace CSharp_Programming
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variables
+            int empHrs = 0;
+            int empwage = 0;
+            Random random new Random();
+            //Computation
+            int empCheck = random. Next(0, 3); 
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                     empHrs = 4;
+                     break;
+
+                case IS_FULL_TIME:
+                     empHrs = 8;
+                     break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empwage = empHrs * EMP_RATE_PER_HOUR; 
+            Console.WriteLine("Emp Wage: " + empwage);
         }
     }
 }
